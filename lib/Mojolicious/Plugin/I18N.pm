2ef7aadf3f868354eedf30ba840ce95d410a3708
Анатолий Шарифулин <sharifulin@gmail.com>
2013-12-25 09:29:42 +0000

    Import of SHARIFULN/Mojolicious-Plugin-I18N-1.1 from CPAN.
    gitpan-cpan-distribution: Mojolicious-Plugin-I18N
    gitpan-cpan-version:      1.1
    gitpan-cpan-path:         SHARIFULN/Mojolicious-Plugin-I18N-1.1.tar.gz
    gitpan-cpan-author:       SHARIFULN
    gitpan-cpan-maturity:     released

--- lib/Mojolicious/Plugin/I18N.pm
+++ lib/Mojolicious/Plugin/I18N.pm
@@ -5,7 +5,7 @@ use Mojo::URL;
 use I18N::LangTags;
 use I18N::LangTags::Detect;
 
-our $VERSION = '1.0';
+our $VERSION = '1.1';
 
 # "Can we have Bender burgers again?
 #  No, the cat shelter’s onto me."
@@ -104,29 +104,40 @@ sub register {
 	});
 	
 	# Reimplement "url_for" helper
-	$app->helper(url_for => sub {
-		my $self = shift;
-		my $url  = $self->url_for(@_);
+	my $mojo_url_for = *Mojolicious::Controller::url_for{CODE};
+	
+	my $i18n_url_for = sub {
+		my ( $self, $target, %params ) = @_;
 		
-		# detect lang
+		my $url = $self->$mojo_url_for($target, %params);
 		
-		my $lang = $self->stash('lang');
-		my $i; for (@_) {
-			$i++;
-			if ($_ eq 'lang'){
-				$lang = $_[$i];
-				last;                                                                                                                                                                                 
-			}   
-		}   
-		
-		if ($lang) {
-			my $str = $url->path;
-			my $new = "/$lang" . ($str eq '/' ? '' : $str);
-			$url->path( $new );
+		# Absolute URL
+		if ( $url->is_abs() ) {
+			return $url;
+		}
+			
+		# Detect lang
+		if ( my $lang = $params{'lang'} || $self->stash('lang') ) {
+			my $path = $url->path();
+			
+			# Root
+			if ( not $path->[0] ) {
+				$path->parts([ $lang ]);
+			# No language detected
+			} elsif ( ref $langs ne 'ARRAY' or not scalar grep { $path->contains("/$_") } @{ $langs } ) {
+				unshift @{ $path->parts() }, $lang;
+			}
 		}
 		
-		$url;
-	});
+		return $url;
+	};
+	
+	{
+		no strict 'refs';
+		no warnings 'redefine';
+		
+		*Mojolicious::Controller::url_for = $i18n_url_for;
+	}
 }
 
 package Mojolicious::Plugin::I18N::_Handler;
@@ -366,7 +377,7 @@ L<http://search.cpan.org/dist/Mojolicious-Plugin-I18N>
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright (C) 2011-2013 by Anatoly Sharifulin.
+Copyright (C) 2011-2014 by Anatoly Sharifulin.
 Copyright (C) 2008-2012, Sebastian Riedel.
 
 This program is free software, you can redistribute it and/or modify it under
